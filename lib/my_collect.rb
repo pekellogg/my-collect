484e6af34b867625928a548b40493cf65d7cee0b
Peyton <pekellogg@hotmail.com>
2021-09-13 14:14:55 -0700

    commit tree df69cc4c6f2eb7885f0aefc476d1afcf62e38a47

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(collection)
+    if block_given?
+        i = 0
+        container = []
+        while i < collection.length
+            container << yield(collection[i])
+            i += 1
+        end
+        container
+    else
+        "Error: no block given."
+    end
+end
 
-
+# family = ["shanks", "pey", "simbo"]
+# my_collect(family) { |i| i.upcase }
